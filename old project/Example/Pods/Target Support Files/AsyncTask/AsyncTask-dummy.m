898c387d4745bc8db28e0ab02fb20146c4d92292
Zhixuan Lai <zhxnlai@gmail.com>
2016-06-20 19:22:25 -0700

    delete old project

--- old project/Example/Pods/Target Support Files/AsyncTask/AsyncTask-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AsyncTask : NSObject
-@end
-@implementation PodsDummy_AsyncTask
-@end
--- old project/Example/Pods/Target Support Files/Pods-AsyncTask_Tests/Pods-AsyncTask_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_AsyncTask_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_AsyncTask_TestsVersionString[];
-
